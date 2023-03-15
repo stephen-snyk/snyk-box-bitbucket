194ab324d0d00bcc4503b3ec3460cb47c330635c
freshdemo <110042227+stephen-snyk@users.noreply.github.com>
2023-03-15 09:43:29 -0400

    Retooling another repo

--- firewallrules.tf
+++ firewallrules.tf
@@ -1,41 +1,17 @@
-# Filters for Firewall Rules to use
-resource "cloudflare_filter" "challenge" {
-  zone_id = var.cloudflare_zone_id
-  description = "Can be used to demo JS challnege at example.com/challenge."
-  expression = "(http.host eq \"${var.cloudflare_zone}\" and http.request.uri.path eq \"/challenge\")"
+resource "google_project" "snyk-cx-se-demo" {
+  name       = "tf-test-project"
+  project_id = "ae-project"
+  org_id     = "123456789"
 }
 
-resource "cloudflare_filter" "captcha" {
-  zone_id = var.cloudflare_zone_id
-  description = "Can be used to demo captcha at example.com/captcha."
-  expression = "(http.host eq \"${var.cloudflare_zone}\" and http.request.uri.path eq \"/captcha\")"
+resource "google_app_engine_application" "app" {
+  project     = google_project.my_project.project_id
+  location_id = "us-central"
 }
 
-resource "cloudflare_filter" "apiRLbypass" {
-  zone_id = var.cloudflare_zone_id
-  description = "Can be used to demo captcha at example.com/captcha."
-  expression = "(http.host eq \"api.${var.cloudflare_zone}\") and (any(http.request.headers.values[*] contains \"bypass\"))"
-}
-
-# Firewall rules to use filters with
-resource "cloudflare_firewall_rule" "challenge" {
-  zone_id = var.cloudflare_zone_id
-  description = "JS Challenge Demo"
-  filter_id = cloudflare_filter.challenge.id
-  action = "js_challenge"
-}
-
-resource "cloudflare_firewall_rule" "captcha" {
-  zone_id = var.cloudflare_zone_id
-  description = "Captcha Challenge Demo"
-  filter_id = cloudflare_filter.captcha.id
-  action = "challenge"
-}
-
-resource "cloudflare_firewall_rule" "apiRLbypass" {
-  zone_id = var.cloudflare_zone_id
-  description = "Bypass API Rate Limiting"
-  filter_id = cloudflare_filter.apiRLbypass.id
-  action = "bypass"
-  products = [ "rateLimit" ]
+resource "google_app_engine_firewall_rule" "rule" {
+  project      = google_app_engine_application.app.project
+  priority     = 1000
+  action       = "ALLOW"
+  source_range = "*"
 }
